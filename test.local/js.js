209c5ec6f89cbcd9b32d8dd7eb0ba6e4573e5642
Юлия Попова <yuliy13602@gmail.com>
2026-06-04 22:29:55 +0300

    version 1.0
    Тут 3д модели которые можно крутить вертеть и добавлять новые

---
+++ test.local/js.js
@@ -0,0 +1,7 @@
+if radioButton1.Checked=true then
+      <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js" ar environment-image="shared-assets/environments/moon_1k.hdr" poster="shared-assets/models/NeilArmstrong.webp"></script>
+      <model-viewer src="pencil.glb" alt="3D модель" auto-rotate camera-controls></model-viewer>
+    else
+      <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js" ar environment-image="shared-assets/environments/moon_1k.hdr" poster="shared-assets/models/NeilArmstrong.webp"></script>
+      <model-viewer src="bee.glb" alt="3D модель" auto-rotate camera-controls></model-viewer>
+    endIf
